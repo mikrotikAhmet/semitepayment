75d0d8e46d25c62cbdc9686ac883c981dae4d0b8
Ahmet GUDENOGLU <ahmet.gudenoglu@gmail.com>
2014-06-28 19:58:30 +0200

    API Started

---
+++ api/controller/semite/api.php
@@ -0,0 +1,65 @@
+<?php
+
+if (!defined('DIR_APPLICATION'))
+    exit('No direct script access allowed');
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.1.6 or newer
+ *
+ * @package		CodeIgniter
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2008 - 2011, EllisLab, Inc.
+ * @license		http://codeigniter.com/user_guide/license.html
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
+// ------------------------------------------------------------------------
+
+/*
+ * Copyright (C) 2014 ahmet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Semite LLC api Class
+ * Date : Jun 28, 2014
+ */
+
+class ControllerSemiteApi extends Controller{
+    
+    private $proxy = array();
+
+
+    public function index(){
+        
+        
+        $this->proxy[] = array(
+            'id'=>1,
+            'name'=>'Ahmet Gudenoglu'
+        );
+        
+        $this->data['proxy'] = json_encode($this->proxy);
+        
+        $this->template = 'semite/api.tpl';
+
+        $this->response->setOutput($this->render());
+        
+        
+    }
+}
+
---
+++ system/config/api.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.1.6 or newer
+ *
+ * @package		CodeIgniter
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2008 - 2011, EllisLab, Inc.
+ * @license		http://codeigniter.com/user_guide/license.html
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * @package     Semite LLC
+ * @version     $Id: api.php Jun 28, 2014 ahmet
+ * @copyright   Copyright (c) 2014 Semite LLC .
+ * @license     http://www.semitepayment.com/license/
+ */
+/**
+ * Description of api.php
+ *
+ * @author ahmet
+ */
+
+// HTTP
+define('HTTP_SERVER', 'http://local.semitepayment.com/api/');
+define('HTTP_PUBLIC', 'http://local.semitepayment.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://local.semitepayment.com/api/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/semitepayment.com/api/');
+define('DIR_SYSTEM', '/var/www/html/semitepayment.com/system/');
+define('DIR_LIBRARY', '/var/www/html/semitepayment.com/system/library/');
+define('DIR_DATABASE', '/var/www/html/semitepayment.com/system/database/');
+define('DIR_TEMPLATE', '/var/www/html/semitepayment.com/api/view/template/');
+define('DIR_LANGUAGE', '/var/www/html/semitepayment.com/admin/language/');
+define('DIR_CONFIG', '/var/www/html/semitepayment.com/system/library/config/');
+define('DIR_CACHE', '/var/www/html/semitepayment.com/system/cache/');
+define('DIR_LOGS', '/var/www/html/semitepayment.com/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'semitepayment');
+define('DB_PASSWORD', 's3m1t3P@ym3nT');
+define('DB_DATABASE', 'semitepayment');
+define('DB_PREFIX', 'engine4_');
+
