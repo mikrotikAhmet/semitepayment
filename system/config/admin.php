f13a28041da54356bec8485bbd29e31cceac7aff
root <root@egaming.localdomain>
2014-06-30 12:00:44 +0200

    updating gitignore

--- system/config/admin.php
+++ system/config/admin.php
@@ -20,7 +20,7 @@
  * @package     Semite LLC
  * @version     $Id: admin.php Jun 14, 2014 ahmet
  * @copyright   Copyright (c) 2014 Semite LLC .
- * @license     http://www.semitepayment.com/license/
+ * @license     http://local.semitepayment.com/license/
  */
 /**
  * Description of admin.php
@@ -28,34 +28,34 @@
  * @author ahmet
  */
 // HTTP
-define('HTTP_SERVER', 'http://www.semitepayment.com/admin/');
-define('HTTP_PUBLIC', 'http://www.semitepayment.com/');
-define('HTTP_IMAGE', 'http://www.semitepayment.com/image/');
+define('HTTP_SERVER', 'http://local.semitepayment.com/admin/');
+define('HTTP_PUBLIC', 'http://local.semitepayment.com/');
+define('HTTP_IMAGE', 'http://local.semitepayment.com/image/');
 define('HTTP_DOCUMENTATION', 'http://docs.semitepayment.com/');
 define('HTTP_SUPPORT', 'http://form.semitepayment.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://www.semitepayment.com/admin/');
-define('HTTPS_PUBLIC', 'https://www.semitepayment.com/');
-define('HTTPS_IMAGE', 'https://www.semitepayment.com/image/');
+define('HTTPS_SERVER', 'https://local.semitepayment.com/admin/');
+define('HTTPS_PUBLIC', 'https://local.semitepayment.com/');
+define('HTTPS_IMAGE', 'https://local.semitepayment.com/image/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/semitepayment.com/admin/');
-define('DIR_SYSTEM', '/var/www/html/semitepayment.com/system/');
-define('DIR_LIBRARY', '/var/www/html/semitepayment.com/system/library/');
-define('DIR_DATABASE', '/var/www/html/semitepayment.com/system/database/');
-define('DIR_TEMPLATE', '/var/www/html/semitepayment.com/admin/view/template/');
-define('DIR_LANGUAGE', '/var/www/html/semitepayment.com/admin/language/');
-define('DIR_CONFIG', '/var/www/html/semitepayment.com/system/library/config/');
-define('DIR_IMAGE', '/var/www/html/semitepayment.com/image/');
-define('DIR_CACHE', '/var/www/html/semitepayment.com/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/semitepayment.com/download/');
-define('DIR_LOGS', '/var/www/html/semitepayment.com/system/logs/');
-define('DIR_PUBLIC', '/var/www/html/semitepayment.com/public/');
+define('DIR_APPLICATION', '/var/local/html/semitepayment.com/admin/');
+define('DIR_SYSTEM', '/var/local/html/semitepayment.com/system/');
+define('DIR_LIBRARY', '/var/local/html/semitepayment.com/system/library/');
+define('DIR_DATABASE', '/var/local/html/semitepayment.com/system/database/');
+define('DIR_TEMPLATE', '/var/local/html/semitepayment.com/admin/view/template/');
+define('DIR_LANGUAGE', '/var/local/html/semitepayment.com/admin/language/');
+define('DIR_CONFIG', '/var/local/html/semitepayment.com/system/library/config/');
+define('DIR_IMAGE', '/var/local/html/semitepayment.com/image/');
+define('DIR_CACHE', '/var/local/html/semitepayment.com/system/cache/');
+define('DIR_DOWNLOAD', '/var/local/html/semitepayment.com/download/');
+define('DIR_LOGS', '/var/local/html/semitepayment.com/system/logs/');
+define('DIR_PUBLIC', '/var/local/html/semitepayment.com/public/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'semite.cvv87auvsvsm.us-west-2.rds.amazonaws.com');
+define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'semitepayment');
 define('DB_PASSWORD', 's3m1t3P@ym3nT');
 define('DB_DATABASE', 'semitepayment');
--- system/config/public.php
+++ system/config/public.php
@@ -20,7 +20,7 @@
  * @package     Semite LLC
  * @version     $Id: public.php Jun 16, 2014 ahmet
  * @copyright   Copyright (c) 2014 Semite LLC .
- * @license     http://www.semitepayment.com/license/
+ * @license     http://local.semitepayment.com/license/
  */
 /**
  * Description of public.php
@@ -28,33 +28,33 @@
  * @author ahmet
  */
 // HTTP
-define('HTTP_SERVER', 'http://www.semitepayment.com/');
-define('HTTP_PUBLIC', 'http://www.semitepayment.com/');
-define('HTTP_IMAGE', 'http://www.semitepayment.com/image/');
+define('HTTP_SERVER', 'http://local.semitepayment.com/');
+define('HTTP_PUBLIC', 'http://local.semitepayment.com/');
+define('HTTP_IMAGE', 'http://local.semitepayment.com/image/');
 define('HTTP_DOCUMENTATION', 'http://docs.semitepayment.com/');
 define('HTTP_SUPPORT', 'http://form.semitepayment.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://www.semitepayment.com/');
-define('HTTPS_PUBLIC', 'https://www.semitepayment.com/');
-define('HTTPS_IMAGE', 'https://www.semitepayment.com/image/');
+define('HTTPS_SERVER', 'https://local.semitepayment.com/');
+define('HTTPS_PUBLIC', 'https://local.semitepayment.com/');
+define('HTTPS_IMAGE', 'https://local.semitepayment.com/image/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/semitepayment.com/public/');
-define('DIR_SYSTEM', '/var/www/html/semitepayment.com/system/');
-define('DIR_LIBRARY', '/var/www/html/semitepayment.com/system/library/');
-define('DIR_DATABASE', '/var/www/html/semitepayment.com/system/database/');
-define('DIR_LANGUAGE', '/var/www/html/semitepayment.com/public/language/');
-define('DIR_TEMPLATE', '/var/www/html/semitepayment.com/public/view/theme/');
-define('DIR_CONFIG', '/var/www/html/semitepayment.com/system/library/config/');
-define('DIR_IMAGE', '/var/www/html/semitepayment.com/image/');
-define('DIR_CACHE', '/var/www/html/semitepayment.com/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/semitepayment.com/download/');
-define('DIR_LOGS', '/var/www/html/semitepayment.com/system/logs/');
+define('DIR_APPLICATION', '/var/local/html/semitepayment.com/public/');
+define('DIR_SYSTEM', '/var/local/html/semitepayment.com/system/');
+define('DIR_LIBRARY', '/var/local/html/semitepayment.com/system/library/');
+define('DIR_DATABASE', '/var/local/html/semitepayment.com/system/database/');
+define('DIR_LANGUAGE', '/var/local/html/semitepayment.com/public/language/');
+define('DIR_TEMPLATE', '/var/local/html/semitepayment.com/public/view/theme/');
+define('DIR_CONFIG', '/var/local/html/semitepayment.com/system/library/config/');
+define('DIR_IMAGE', '/var/local/html/semitepayment.com/image/');
+define('DIR_CACHE', '/var/local/html/semitepayment.com/system/cache/');
+define('DIR_DOWNLOAD', '/var/local/html/semitepayment.com/download/');
+define('DIR_LOGS', '/var/local/html/semitepayment.com/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'semite.cvv87auvsvsm.us-west-2.rds.amazonaws.com');
+define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'semitepayment');
 define('DB_PASSWORD', 's3m1t3P@ym3nT');
 define('DB_DATABASE', 'semitepayment');
