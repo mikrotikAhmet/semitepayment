eb651bf59c4ca9f921748c0d4527df8be6615a14
root <root@egaming.localdomain>
2014-07-21 15:46:45 +0200

    Notifying Customer by Mail

--- admin/model/account/transaction.php
+++ admin/model/account/transaction.php
@@ -296,20 +296,20 @@ class ModelAccountTransaction extends Model {
 //
 //			$message .= $language->get('text_footer');
 //
-//			$mail = new Mail();
-//			$mail->protocol = $this->config->get('config_mail_protocol');
-//			$mail->parameter = $this->config->get('config_mail_parameter');
-//			$mail->hostname = $this->config->get('config_smtp_host');
-//			$mail->username = $this->config->get('config_smtp_username');
-//			$mail->password = $this->config->get('config_smtp_password');
-//			$mail->port = $this->config->get('config_smtp_port');
-//			$mail->timeout = $this->config->get('config_smtp_timeout');
-//			$mail->setTo($withdraw_info['email']);
-//			$mail->setFrom($this->config->get('config_email'));
-//			$mail->setSender($withdraw_info['store_name']);
-//			$mail->setSubject(html_entity_decode($subject, ENT_QUOTES, 'UTF-8'));
-//			$mail->setText(html_entity_decode($message, ENT_QUOTES, 'UTF-8'));
-//			$mail->send();
+			$mail = new Mail();
+			$mail->protocol = $this->config->get('config_mail_protocol');
+			$mail->parameter = $this->config->get('config_mail_parameter');
+			$mail->hostname = $this->config->get('config_smtp_host');
+			$mail->username = $this->config->get('config_smtp_username');
+			$mail->password = $this->config->get('config_smtp_password');
+			$mail->port = $this->config->get('config_smtp_port');
+			$mail->timeout = $this->config->get('config_smtp_timeout');
+			$mail->setTo('ahmet.gudenoglu@gmail.com');
+			$mail->setFrom('test@semitepayment.com');
+			$mail->setSender('Semite Payment');
+			$mail->setSubject(html_entity_decode('Fund Transfer', ENT_QUOTES, 'UTF-8'));
+			$mail->setText(html_entity_decode('You are rich ha!', ENT_QUOTES, 'UTF-8'));
+			$mail->send();
 		}
 		
 	}
