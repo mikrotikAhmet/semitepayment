d04a91fcd436d2a237c424a83ed5d5ce986d546e
root <root@egaming.localdomain>
2014-07-28 10:57:17 +0200

    Mail Test

--- admin/controller/common/home.php
+++ admin/controller/common/home.php
@@ -218,22 +218,6 @@ class ControllerCommonHome extends Controller {
             $this->model_localisation_currency->updateCurrencies();
         }
         
-        // Remove This Later
-        $mail = new Mail();
-            $mail->protocol = $this->config->get('config_mail_protocol');
-            $mail->parameter = $this->config->get('config_mail_parameter');
-            $mail->hostname = $this->config->get('config_smtp_host');
-            $mail->username = $this->config->get('config_smtp_username');
-            $mail->password = $this->config->get('config_smtp_password');
-            $mail->port = $this->config->get('config_smtp_port');
-            $mail->timeout = $this->config->get('config_smtp_timeout');
-            $mail->setTo('ahmet.gudenoglu@gmail.com');
-            $mail->setFrom($this->config->get('config_email'));
-            $mail->setSender($this->config->get('config_name'));
-            $mail->setSubject(html_entity_decode(sprintf('Your account approved by %s', $this->config->get('config_name')), ENT_QUOTES, 'UTF-8'));
-            $mail->setText(html_entity_decode('Good Luck', ENT_QUOTES, 'UTF-8'));
-            $mail->send();
-
         $this->template = 'common/home.tpl';
         $this->children = array(
             'common/header',
