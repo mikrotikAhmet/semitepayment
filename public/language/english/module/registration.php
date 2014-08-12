2bca291290e8b5b8b878697ae3cf47bb6c0c2cd3
Ahmet GUDENOGLU <ahmet.gudenoglu@gmail.com>
2014-08-12 19:18:13 +0200

    Registration error translations

--- public/language/english/module/registration.php
+++ public/language/english/module/registration.php
@@ -29,3 +29,9 @@
  */
 
 
+// Error
+$_['error_firstname']       = 'First Name must be between 1 and 32 characters!';
+$_['error_lastname']        = 'Last Name must be between 1 and 32 characters!';
+$_['error_email'] = 'E-Mail Address does not appear to be valid!';
+$_['error_password']        = 'Password must be between 4 and 20 characters!';
+
